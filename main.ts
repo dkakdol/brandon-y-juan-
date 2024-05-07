7a169234bfc7cf02a8590bdf938d608398a8d398
dkakdol <juandiegestudiante@gmail.com>
2024-05-07 13:20:34 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.showString("Brandon Ossa B.")
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.showString("Juan Diego o.")
+    basic.clearScreen()
+})
